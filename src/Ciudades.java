a666c51076fb26cce3e16dd7836c76766be350d3
Gerardo Guinez <gerardoguinezproyectopoo@gmail.com>
2026-06-30 19:21:58 -0400

    avancé :v

--- src/Ciudades.java
+++ src/Ciudades.java
@@ -1,20 +1,19 @@
-import java.awt.*;
-
-
 public class Ciudades {
-    //Gerardo Guíñez y Sebastián ...
-    int[][] grafoDestinos;
-    String[] nombreCiudad;
-    //boolean gate;
-    int N;
-    //Use la misma estructra del lab pq se ven similares
+    //Gerardo Guíñez y Sebastián Ancan
+
+    //atributos
+    private int[][] grafoDestinos;
+    private String[] nombreCiudad;
+    private int N;
+    //Usamos la misma estructra del lab por que se ven similares
+
+    //constructor
     Ciudades(){
         this.N = 100;
         this.grafoDestinos = new int[N][N];
         this.nombreCiudad = new String[N];
         inicializar();
     }
-
     void inicializar(){
         for(int i = 0;i <N;i++){
             for (int j = 0; j<N;j++){
@@ -23,63 +22,48 @@ public class Ciudades {
         }
     }
 
+    //Métodos
     boolean registrarCiudad(int id, String  nombre){
-        if (id < 0 || id >= N) {
-            return false;
-        }
-        if (nombreCiudad[id] != null){
-            return false;
-        }
+        if(id < 0 || id >= N) return false;
+
+        if(nombreCiudad[id] != null) return false;
 
         nombreCiudad[id] = nombre;
         return true;
     }
 
     boolean registrarRuta(int idOrigen,int idDestino,int costo){
-        if (!verificadores(idOrigen,idDestino)){
-            return false;
-        }
-        if (nombreCiudad[idDestino] == null || nombreCiudad[idOrigen] == null){
-            return false;
-        }
+        if(!verificadores(idOrigen,idDestino)) return false;
 
-        if (grafoDestinos[idOrigen][idDestino] != Integer.MAX_VALUE){
-            return false;
-        }
-        if (costo < 0){
-            return false;
-        }
+        if(nombreCiudad[idDestino] == null || nombreCiudad[idOrigen] == null) return false;
+
+        if(grafoDestinos[idOrigen][idDestino] != Integer.MAX_VALUE) return false;
+
+        if(costo < 0) return false;
 
         grafoDestinos[idOrigen][idDestino] = costo;
         return true;
     }
 
-    boolean verificadores(int idOrigen,int idDestino){
-        if (idOrigen < 0 || idOrigen >= N){
-            return false;
-        }
-        if (idDestino <0 || idDestino >= N){
-            return false;
-        }
-        if (idDestino == idOrigen){
-            return false;
-        }
+    private boolean verificadores(int idOrigen,int idDestino){
+        if(idOrigen < 0 || idOrigen >= N) return false;
+
+        if(idDestino <0 || idDestino >= N) return false;
+
+        if(idDestino == idOrigen) return false;
 
         return true;
+        //credito a mi amigo seba por pensar en esta genialidad
     }
 
     int costoRuta(int idOrigen, int idDestino) {
-        if (grafoDestinos[idOrigen][idDestino] == Integer.MAX_VALUE){
-            return Integer.MAX_VALUE;
-        }else {
-            return grafoDestinos[idOrigen][idDestino];
-        }
+        if(!verificadores(idOrigen,idDestino)) return Integer.MAX_VALUE;
+
+        return grafoDestinos[idOrigen][idDestino];
     }
 
     boolean existeConexion(int idOrigen,int idDestino){
-        if (!verificadores(idOrigen,idDestino)){
-            return false;
-        }
+        if(!verificadores(idOrigen,idDestino)) return false;
 
         int[] visitados = new int[N];
 
@@ -91,16 +75,13 @@ public class Ciudades {
     }
 
     private boolean buscar(int idOrigen,int idDestino,int[] visitados){
-        if (idOrigen == idDestino){
-            return true; //Llegamo
-        }
+        if(idOrigen == idDestino) return true; //Llegamo
 
-        visitados[idOrigen] = 1; //visitao
-        for (int i = 0; i< N;i++){
-            if (grafoDestinos[idOrigen][i] != Integer.MAX_VALUE && visitados[i] == 0){
-                if (buscar(i,idDestino,visitados)){
-                    return true;
-                }
+        visitados[idOrigen] = 1; //visitado
+
+        for(int i = 0; i< N;i++){
+            if(grafoDestinos[idOrigen][i] != Integer.MAX_VALUE && visitados[i] == 0){
+                if (buscar(i, idDestino,visitados)) return true;
             }
         }
         return false;
@@ -108,37 +89,38 @@ public class Ciudades {
 
     //implementar algoritmo de Djriska para la distancia mas corta
     public void dijkstra(int idOrigen,int idDestino){
-        if (!verificadores(idOrigen,idDestino)){
+        if(!verificadores(idOrigen,idDestino)){
             System.out.println("invalido");
             return;
         }
+
         int[] distancias = new int[N];
         int[] visitado = new int[N];
         int[] padre = new int[N];
-        int i = 0;
-        for (i = 0;i<N;i++){
+
+        for(int i = 0;i<N;i++){
             distancias[i] = Integer.MAX_VALUE;
             visitado[i] = 0;//0 false, 1 true
             padre[i] = -1; //no predecesor
         }
         distancias[idOrigen] = 0;
 
-        for (i = 0; i<N;i++){ //buscamos q no hayamos visitado
+        for(int i = 0; i<N;i++){ //buscamos q no hayamos visitado
             int x = -1;
             int minimo = Integer.MAX_VALUE;
-            for (int j = 0; j<N;j++){
-                if (visitado[j] == 0 && distancias[j] < minimo){
+            for(int j = 0; j<N;j++){
+                if (visitado[j] == 0 && distancias[j]< minimo){
                     minimo = distancias[j];
                     x = j;
                 }
             }
 
-            //no encontramo o ya llegamo
-            if (x == -1 || x == idDestino){
-                break;
-            }
+            //no encontramos o ya llegamo
+            if(x == -1 || x == idDestino) break;
+
             visitado[x] = 1;
-            for (int v = 0 ; v<N;v++){
+
+            for(int v = 0 ; v<N;v++){
                 if (visitado[v] == 0 && grafoDestinos[x][v] != Integer.MAX_VALUE){
                     int costo = distancias[x] + grafoDestinos[x][v];
                     if (costo < distancias[v]){
@@ -148,8 +130,7 @@ public class Ciudades {
                 }
             }
         }
-        if (distancias[idDestino] == Integer.MAX_VALUE)
-        {
+        if(distancias[idDestino] == Integer.MAX_VALUE){
             System.out.println("No existe un camino :c");
         }else{
             imprimirCaminoCorto(idOrigen,idDestino,padre,distancias[idDestino]);
@@ -157,55 +138,54 @@ public class Ciudades {
     }
     //Agregar coso para imprimir el arreglo
     public void imprimirCaminoCorto(int idOrigen,int idDestino,int[] padre,int distanciaMin){
-        System.out.println("Distancia minima para ir de la ciudad " + idOrigen + " A " + idDestino + " es " + distanciaMin);
+        System.out.println("Distancia minima para ir de la ciudad " + idOrigen + " a " + idDestino + " es " + distanciaMin);
         String ruta = "";
-        int actual = idDestino;
-        while (actual != -1){
-            if (ruta.equals("")){
-                ruta = String.valueOf(actual);
+        int aux = idDestino;
+
+        while(aux != -1){
+            if(ruta.isEmpty()){
+                ruta = String.valueOf(aux);
             }else{
-                ruta = actual + "-> " + ruta;
+                ruta = aux + "-> " + ruta;
             }
-            actual = padre[actual];
+
+            aux = padre[aux];
         }
+
         System.out.println("El camino para ir de la ciudad " + idOrigen + " a la " + idDestino + " es: " + ruta);
-
-
     }
 
-    //Parte 2 de la tarea, hola gerardo , Lo voy a dejar hecho antes de la hospitalizacion :P
-    private void djsristrakAux(int idOrigen,int[] padre, int[] distancia){
+    private void dijkstraAux(int idOrigen, int[] padre, int[] distancia){
         int[] visitado = new int[N];
 
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++){//inicializar
             distancia[i] = Integer.MAX_VALUE;
             visitado[i] = 0;
             padre[i] = -1;
         }
+
         distancia[idOrigen] = 0;
 
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++){//asignar
             int x = -1;
             int minimo = Integer.MAX_VALUE;
 
-            for (int j = 0; j < N; j++) {
-                if (visitado[j] == 0 && distancia[j] < minimo) {
+            for(int j = 0; j < N; j++) {
+                if(visitado[j] == 0 && distancia[j] < minimo){
                     minimo = distancia[j];
                     x = j;
                 }
             }
 
             //no hay nodos alcanzables
-            if (x == -1) {
-                break;
-            }
+            if(x == -1) break;
 
             visitado[x] = 1;
 
-            for (int v = 0; v < N; v++) {
-                if (visitado[v] == 0 && grafoDestinos[x][v] != Integer.MAX_VALUE) {
+            for(int v = 0; v < N; v++){
+                if(visitado[v] == 0 && grafoDestinos[x][v] != Integer.MAX_VALUE){
                     int costo = distancia[x] + grafoDestinos[x][v];
-                    if (costo < distancia[v]) {
+                    if(costo <distancia[v]){
                         distancia[v] = costo;
                         padre[v] = x;
                     }
@@ -215,54 +195,52 @@ public class Ciudades {
     }
 
     public void ciudadGate(int k){
-        if (k <= 0 || k>N){
+        if(k <= 0 || k>N){
             System.out.println("valor invalido");
             return;
         }
 
         int[] conteo = new int[N];
 
-        for (int origen = 0 ; origen<N;origen++) {
-            if (nombreCiudad[origen] == null) {
-                continue;
-            }
+        for(int origen = 0 ; origen<N;origen++) {
+            if(nombreCiudad[origen] == null) continue;
+
             int[] padre = new int[N];
             int[] distancia = new int[N];
 
-            djsristrakAux(origen, padre, distancia);
+            dijkstraAux(origen, padre, distancia);
 
-            for (int destino = 0; destino < N; destino++) {
-                if (origen == destino || nombreCiudad[destino] == null || distancia[destino] == Integer.MAX_VALUE) {
-                    continue;
-                }
+            for(int destino = 0; destino < N; destino++){
+                if(origen == destino || nombreCiudad[destino] == null || distancia[destino] == Integer.MAX_VALUE) continue;
+
                 int actual = padre[destino];
-                while (actual != -1 && actual != origen) {
+                while(actual != -1 &&actual != origen){
                     conteo[actual]++; // 1 mas de trafico
                     actual = padre[actual];
                 }
             }
         }
-                System.out.println("\n--- TOP " + k + " CIUDADES GATE ---");
-                for (int i = 0; i < k; i++) {
-                    int maxConteo = -1;
-                    int idMax = -1;
 
-                    // ciudad mas buscada
-                    for (int j = 0; j < N; j++) {
-                        if (nombreCiudad[j] != null && conteo[j] > maxConteo) {
-                            maxConteo = conteo[j];
-                            idMax = j;
-                        }
-                    }
+        System.out.println("\nTop " + k + " Ciudades GATE ");
 
-                    if (idMax != -1) {
-                        System.out.println((i + 1) + ". " + nombreCiudad[idMax] + " (ID: " + idMax + ") - Atraviesa " + maxConteo + " rutas óptimas.");
-                        conteo[idMax] = -1;
-                    }
+        for(int i = 0; i < k; i++){
+            int maxConteo = -1;
+            int idMax = -1;
+
+            //Ciudad mas buscada
+            for(int j = 0; j < N; j++){
+                if(nombreCiudad[j] != null && conteo[j] > maxConteo){
+                    maxConteo = conteo[j];
+                    idMax = j;
                 }
+            }
 
-
+            if(idMax != -1){
+                System.out.println((i + 1) + ", " + nombreCiudad[idMax] + " (ID: " + idMax + ") - Atraviesa " + maxConteo + " rutas óptimas");
+                conteo[idMax] = -1;
+            }
         }
     }
+}
 
 
--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,15 @@ public class Main {
 
 
     static void menu(){
-        while (true){
-            int opcion;
+        int opcion;
+        while(true){
             System.out.println("1- REGISTRAR CIUDAD");
             System.out.println("2- REGISTRAR RUTA");
             System.out.println("3- BUSCAR CONEXION ENTRE CIUDADES");
-            System.out.println("4- Buscar el camino mas corto entre 2 ciudades");
-            System.out.println("5- Calcular mejores ciudades Gate");
-            System.out.println("6- Usar datos pre-definidos");
-            System.out.println("7- Salir");
+            System.out.println("4- BUSCAR EL CAMINO MAS CORTO ENTRE CIUDADES");
+            System.out.println("5- CALCULAR MEJORES CIUDADES GATE");
+            System.out.println("6- USAR DATOS PRE-DEFINIDOS");
+            System.out.println("7- SALIR DEL PROGRAMA");
             opcion = tcld.nextInt();
             switch (opcion){
                     case 1:
@@ -39,6 +39,7 @@ public class Main {
                         pruebas();
                         break;
                     case 7:
+                        System.out.println("Chao cuidese :D");
                         return;
                     default:
                         System.out.println("Opcion Invalida");
@@ -52,23 +53,28 @@ public class Main {
         System.out.println("Ingrese ID");
         int id = tcld.nextInt();
         tcld.nextLine();
+
         System.out.println("Ingrese nombre");
         String nombre = tcld.nextLine();
-        if (redCiudad.registrarCiudad(id, nombre)){
-            System.out.println("Ciudad registrada exitosamente.");
-        } else {
+
+        if(redCiudad.registrarCiudad(id, nombre)){
+            System.out.println("Ciudad registrada exitosamente");
+        }else {
             System.out.println("Error: No se pudo registrar la ciudad");
         }
     }
 
     static void registrarR(){
-        System.out.println("Ingrese ID origen");
+        System.out.println("Ingresar ID origen");
         int id1 = tcld.nextInt();
-        System.out.println("Ingrese ID destino");
+
+        System.out.println("Ingresar ID destino");
         int id2 = tcld.nextInt();
-        System.out.println("Ingrese costo de la ruta");
+
+        System.out.println("Ingresar costo de la ruta");
         int costo = tcld.nextInt();
-        if (redCiudad.registrarRuta(id1,id2,costo)){
+
+        if(redCiudad.registrarRuta(id1,id2,costo)){
             System.out.println("Registrado exitosamente");
         }else {
             System.out.println("Error al registrar");
@@ -76,64 +82,70 @@ public class Main {
 
     }
     static void buscarCon(){
-        System.out.println("Ingrese ID origen");
+        System.out.println("Ingresar ID origen");
         int id1 = tcld.nextInt();
-        System.out.println("Ingrese ID destino");
+
+        System.out.println("Ingresar ID destino");
         int id2 = tcld.nextInt();
-       if (redCiudad.existeConexion(id1,id2)){
+
+       if(redCiudad.existeConexion(id1,id2)){
            System.out.println("Si existe conexion");
        }else {
            System.out.println("No existe conexion");
        }
 
     }
+
     static void calcularDj(){
         System.out.println("Ingrese ID origen");
         int id1 = tcld.nextInt();
+
         System.out.println("Ingrese ID destino");
         int id2 = tcld.nextInt();
+
         redCiudad.dijkstra(id1,id2);
     }
+
     static void BuscarG(){
-        System.out.println("Cuantas ciudades gate quiere buscar");
+        System.out.print("Ingresar la cantidad de ciudades gate que quiere buscar: ");
         int k = tcld.nextInt();
+
         System.out.println("\nCalculando las " + k + " mejores ciudades gate");
         redCiudad.ciudadGate(k);
     }
-    static void pruebas(){
 
-        System.out.println("--- REGISTRO DE CIUDADES ---");
+    static void pruebas(){
+        System.out.println("Registro ciudades");
         System.out.println("Registrar Cauquenes (ID 0): " + redCiudad.registrarCiudad(0, "Cauquenes"));
         System.out.println("Registrar Chillán (ID 1): " + redCiudad.registrarCiudad(1, "Chillán"));
         System.out.println("Registrar Santiago (ID 2): " + redCiudad.registrarCiudad(2, "Santiago"));
         System.out.println("Registrar San Carlos (ID 3): " + redCiudad.registrarCiudad(3, "San Carlos"));
 
-        // Pruebas de error
+        //Pruebas de error
         System.out.println("Registrar duplicado en ID 1: " + redCiudad.registrarCiudad(1, "Buchupureo"));
         System.out.println("Costo de ruta inexistente (4 a 5): " + redCiudad.costoRuta(4, 5));
 
-        System.out.println("\n--- REGISTRO DE RUTAS ---");
+        System.out.println("\nRegistro Rutas");
 
-        // Cauquenes a Santiago directo cuesta 500
         System.out.println("Ruta Cauquenes -> Santiago (costo 500): " + redCiudad.registrarRuta(0, 2, 500));
 
-        // Pero Cauquenes -> Chillán -> Santiago cuesta 300 en total
+        //Cauquenes -> Chillán -> Santiago cuesta 300 en total, debería dar distinto
         System.out.println("Ruta Cauquenes -> Chillán (costo 100): " + redCiudad.registrarRuta(0, 1, 100));
         System.out.println("Ruta Chillán -> Santiago (costo 200): " + redCiudad.registrarRuta(1, 2, 200));
 
-        System.out.println("Ruta Chillán -> San Carlos (costo 50): " + redCiudad.registrarRuta(1, 3, 50));
+        System.out.println("Ruta Chillán -> San Carlos (costo 50) : " + redCiudad.registrarRuta(1, 3, 50));
 
-        // Pruebas de error en rutas
+        //Pruebas de error en rutas
         System.out.println("Ruta con costo negativo (debe dar false): " + redCiudad.registrarRuta(3, 0, -50));
-        System.out.println("Ruta hacia sí mismo (debe dar false): " + redCiudad.registrarRuta(0, 0, 10));
+        System.out.println("Ruta hacia si mismo (debe dar false): " + redCiudad.registrarRuta(0, 0, 10));
 
-        System.out.println("\n--- PRUEBAS DE CONEXIÓN ---");
+        System.out.println("\nPrueba de conexión");
         System.out.println("Existe conexión desde Cauquenes a San Carlos? 0 a 3: " + redCiudad.existeConexion(0, 3)); // Debe ser true
         System.out.println("Existe conexión desde San Carlos a Cauquenes? 3 a 0: " + redCiudad.existeConexion(3, 0)); // Debe ser false
 
-        System.out.println("\n--- PRUEBA DE DIJKSTRA ---");
-        // Debería preferir dar la vuelta por Chillán (Costo 300) en lugar de ir directo 500
-        System.out.println("Calculando camino mínimo desde Cauquenes 0 a Santiago 2");
+        System.out.println("\nPrueba Dijkstra");
+        //Debería preferir dar la vuelta por Chillán (Costo 300) en lugar de ir directo 500
+        System.out.println("Calculando camino minimo desde Cauquenes 0 a Santiago 2");
         redCiudad.dijkstra(0, 2);
 
         System.out.println("\nCalculando las mejores ciudades gate");
